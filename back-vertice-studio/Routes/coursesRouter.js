7ea9faccb0a067c9ecd21e2852dd09ecb3efe42b
Curro Rando <curro.rb@gmail.com>
2024-10-10 13:51:55 +0200

    coursesRouter

--- back-vertice-studio/Routes/coursesRouter.js
+++ back-vertice-studio/Routes/coursesRouter.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const router = express.Router();
+
+//!Enrutados
+
+const {
+  getAllCourses,
+  createCourses,
+  getCoursesById,
+  updateCoursesById,
+  deleteCoursesById,
+} = require ("../Controllers/coursesController");
+
+//! Rutas
+
+router.get("/", getAllCourses);
+router.post("/createCourses", createCourses);
+// router.get("/:idCourses", getCoursesById);
+// router.patch("/:idCourses", updateCoursesById);
+// router.delete("/:idCourses", deleteCoursesById);
+
+module.exports = router;
