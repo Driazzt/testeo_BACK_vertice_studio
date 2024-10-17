aa0fa036a59a65dcc33e9cff2ffa9cec1abafd9f
Curro Rando <curro.rb@gmail.com>
2024-10-17 10:30:59 +0200

    fix lock error password e implementacion verifyUser

--- back-vertice-studio/Routes/loginRouter.js
+++ back-vertice-studio/Routes/loginRouter.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const passwordValidator = require("../Middlewares/passwordValidator")
 const verifyToken = require("../Middlewares/auth")
-const { login, register, getRefreshToken } = require("../Controllers/loginController"); 
+const { login, register, verifyUser, getRefreshToken } = require("../Controllers/loginController"); 
 
 
 router.post("/login", login);
 router.post("/register", passwordValidator, register)
+router.get("/verify", verifyUser);
 router.get("/getRefreshToken", verifyToken, getRefreshToken);
 
 module.exports = router;
